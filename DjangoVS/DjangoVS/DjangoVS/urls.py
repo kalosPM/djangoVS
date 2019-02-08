8884cf60a8d91a4caee7690a0b24bc4f2d8e485d
kalospm <kalospm@kalospm>
2019-02-08 01:56:23 -0500

    segundo commit

---
+++ DjangoVS/DjangoVS/DjangoVS/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for DjangoVS.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', DjangoVS.views.home, name='home'),
+    # url(r'^DjangoVS/', include('DjangoVS.DjangoVS.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+#aaa
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
